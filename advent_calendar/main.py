85e7d0d76c4352a9d9f0306ac04f6f090c698782
Adoria298 <bananaorange29@outlook.com>
2017-12-02 19:55:17 +0000

    Added basic advent calendar

---
+++ advent_calendar/main.py
@@ -0,0 +1,12 @@
+#Advent Calendar
+
+import random
+
+rewards = ["A Christmas Dinner!",
+			"Presents!",
+			"Chocolate!",
+			"Oranges"
+]
+
+print("Your reward is: ")
+print(random.choice(rewards))
