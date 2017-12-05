2672079fdcce158ac7069bcca631e7258b23be15
Adoria298 <bananaorange29@outlook.com>
2017-12-05 18:47:49 +0000

    Added date testing and custom colours

--- christmas_countdown/main.py
+++ christmas_countdown/main.py
@@ -2,9 +2,19 @@
 
 from time import localtime
 from datetime import date
+import colorama
+colorama.init()
 
-christmas = date(localtime()[0], 12, 25) #makes christmas the 25th of December this year
-today = date(localtime()[0], 12, 26) #localtime() returns a tuple in the format (year, month, day)
+if (localtime()[2] < 25 and #if it's before the 25th
+	localtime()[1] == 12):
+		christmas = date(localtime()[0], 12, 25) #makes christmas the 25th of December this year
+else: #if it is after christmas this year
+	christmas = date(localtime()[0]+1, 12, 25)
+today = date(localtime()[0], localtime()[1], localtime()[2]) #localtime() returns a tuple in the format (year, month, day)
 
 difference = christmas - today
-print(str(difference)[:-9] + " until Christmas!")
+difference = int(str(difference)[:3].rstrip()) #difference is the first three characters without spaces or newlines to the right
+if localtime()[1] != 12: #if it's not December
+	print(colorama.Fore.RED + str(difference) + " days until Christmas!")
+else:
+	print(colorama.Fore.GREEN + str(difference) + " days until Christmas!")
