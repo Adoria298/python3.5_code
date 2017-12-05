658ca294541b6e1b73f16c2019ecb61f086cf2ca
Adoria298 <bananaorange29@outlook.com>
2017-12-05 18:19:17 +0000

    Added basic christmas countdown

--- advent_calendar/main.py
+++ advent_calendar/main.py
@@ -1,6 +1,19 @@
 #Advent Calendar
 
 import random
+import os
+import sys
+from datetime import date
+from time import localtime
+
+#open file to save information in
+if os.path.exists("advent_save.txt"):
+	save_file = open("advent_save.txt", "a")
+else:
+	save_file = open("advent_save.txt", "a")
+
+#change where prints direct to
+sys.stdout = save_file
 
 rewards = ["A Christmas Dinner!",
 			"Presents!",
@@ -8,5 +21,14 @@ rewards = ["A Christmas Dinner!",
 			"Oranges"
 ]
 
+#not very efficient, but readable
+today_tuple = localtime()
+today_year = today_tuple[0]
+today_month = today_tuple[1]
+today_day = today_tuple[2]
+today = date(today_year, today_month, today_day).isoformat()
+
+print(today)
 print("Your reward is: ")
 print(random.choice(rewards))
+print("\n")
